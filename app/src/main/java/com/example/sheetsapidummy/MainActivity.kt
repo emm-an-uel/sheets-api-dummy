d275f3e1aeebad07345f1bf6dd812f185d12ad15
emmanuel <sunartio.emmanuel@gmail.com>
2022-12-14 09:42:46 +1030

    copied tutorial from 'sheets for developers' website

--- app/src/main/java/com/example/sheetsapidummy/MainActivity.kt
+++ app/src/main/java/com/example/sheetsapidummy/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.example.sheetsapidummy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.client.util.store.FileDataStoreFactory
+import com.google.api.services.sheets.v4.Sheets
+import com.google.api.services.sheets.v4.SheetsScopes
+import com.google.api.services.sheets.v4.model.ValueRange
+import java.io.FileNotFoundException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var listOfPersons: ArrayList<Person>
     lateinit var rvMain: RecyclerView
     lateinit var adapter: RVAdapter
+    lateinit var btnSync: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +39,18 @@ class MainActivity : AppCompatActivity() {
 
         rvMain = findViewById(R.id.rvMain)
         setupRV()
+
+        btnSync = findViewById(R.id.btnSync)
+        btnSync.setOnClickListener {
+            // get data from google sheets
+            val spreadsheetId = "1uKXpevSHQ_qmUrgehFdMXiaPRC2hkMITUv4gAXjujiY"
+            val range = "A2:B6"
+            getDataFromSheets(spreadsheetId, range)
+        }
     }
 
     private fun getData() {
         initDummyData() // for testing only
-        // TODO: get list of persons from sheets
     }
 
     private fun initDummyData() {
@@ -35,5 +63,52 @@ class MainActivity : AppCompatActivity() {
     private fun setupRV() {
         adapter = RVAdapter(listOfPersons)
         rvMain.adapter = adapter
+    }
+
+    private fun getDataFromSheets(spreadsheetId: String, range: String) {
+        val APPLICATION_NAME: String = "Sheets API Dummy"
+        val JSON_FACTORY: JsonFactory = GsonFactory.getDefaultInstance()
+        val TOKENS_DIRECTORY_PATH: String = "tokens"
+        val CREDENTIALS_FILE_PATH: String = "/credentials.json"
+        val SCOPES: List<String> = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY)
+
+        fun getCredentials(HTTP_TRANSPORT: NetHttpTransport): Credential {
+            // load client secrets
+            val i: InputStream =
+                MainActivity::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
+                    ?: throw FileNotFoundException("Resource not found: $CREDENTIALS_FILE_PATH")
+
+            val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(i))
+
+            // build flow and trigger user authorization request
+            val flow: GoogleAuthorizationCodeFlow = GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(FileDataStoreFactory(java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build()
+
+            val receiver: LocalServerReceiver = LocalServerReceiver.Builder().setPort(8888).build()
+            return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
+        }
+
+        // build new authorized api client service
+        val HTTP_TRANSPORT: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
+        val service: Sheets = Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            .setApplicationName(APPLICATION_NAME)
+            .build()
+        val response: ValueRange = service.spreadsheets().values()
+            .get(spreadsheetId, range)
+            .execute()
+        val values: List<List<Any>> = response.getValues()
+        if (values.isEmpty()) {
+            Toast.makeText(this, "Sheets returned empty list", Toast.LENGTH_SHORT).show()
+        } else {
+            for (row: List<Any> in values) {
+                val name = row[0] as String
+                val age = row[1] as Int
+                val person = Person(name, age)
+                listOfPersons.add(person)
+            }
+        }
     }
 }
